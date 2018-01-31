2b0a0f0c84800d19b0c782b5df59ae1a7158d191
Alex Duncan <aed428@nyu.edu>
2018-01-31 12:26:37 -0500

    Updated scripts and adding scripts to camera

--- Assets/LightPositionMapper.cs
+++ Assets/LightPositionMapper.cs
@@ -5,16 +5,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine.UI;
 
-//The color that we're checking for
-public enum PossibleColors{
-	Red,
-	Green,
-	Blue,
-	Cyan,
-	Magenta,
-	Yellow
-}
-
 public class LightPositionMapper : MonoBehaviour {
 	//Get the webcam render texture
 	//Cycle through it to find the center position of the light source
@@ -33,8 +23,6 @@ public class LightPositionMapper : MonoBehaviour {
 	float saturationThreshold = 0.4f;
 	const float SATURATION_INCREMENT = 0.05f;
 
-	public PossibleColors colorToTrack;
-
 	Color trackingColor;
 	Vector3 adjustedColor;
 
@@ -42,8 +30,6 @@ public class LightPositionMapper : MonoBehaviour {
 	int cameraWidthBuffer;
 	int cameraHeightBuffer;
 	const float BUFFER_PERCENTAGE = 0.1f;
-
-	Transform brushTransform;
 
 	const float MIN_X = -9f;
 	const float MAX_X = 9f;
@@ -58,8 +44,6 @@ public class LightPositionMapper : MonoBehaviour {
 	const int MIN_RUN_SATURATION = 30;
 	int minSaturation;
 	int maxSaturation;
-	bool setMin = false;
-	bool setMax = false;
 
 	//test textures
 	[SerializeField] Renderer testTexture;
@@ -77,76 +61,25 @@ public class LightPositionMapper : MonoBehaviour {
 
 		cameraWidthBuffer = Mathf.RoundToInt (webCamTex.width * BUFFER_PERCENTAGE);
 		cameraHeightBuffer = Mathf.RoundToInt (webCamTex.height * BUFFER_PERCENTAGE);
-
-		brushTransform = GameObject.Find ("Brush").GetComponent<Transform> ();
-
+	
 		#if !TEST
 		testTexture.gameObject.SetActive(false);
 		#else
 		testTexture.material.SetFloat("_Buffer", BUFFER_PERCENTAGE);
 		testTexture.material.SetFloat("_SaturationThreshold", saturationThreshold);
-//		outputTexture.material.SetFloat("_SaturationThreshold", saturationThreshold);
 		#endif
-
-		colorToTrack = (PossibleColors)PlayerPrefs.GetInt ("Tracking Color", 0);
-		SetTrackingColor ();
 
 	}
 	
 	// Update is called once per frame
 	void Update () {
 
-		Vector3 position = ReadWebCamData();
-
-		if (numberPixelsAboveThreshold > MIN_RUN_SATURATION/PIXEL_INCREMENT) {
-			brushTransform.localPosition = Vector3.Lerp(brushTransform.localPosition, position, LERP_SPEED * Time.deltaTime);
-			brushTransform.LookAt(position, -brushTransform.parent.forward);
-		}
+		ReadWebCamData();
 
 		AdjustSaturationThreshold ();
-		SetZDepthRange ();
 	}
 
-	void SetTrackingColor(){
-		switch (colorToTrack) {
-		case PossibleColors.Red:
-			trackingColor = Color.red;
-			break;
-		case PossibleColors.Green:
-			trackingColor = Color.green;
-			break;
-		case PossibleColors.Blue:
-			trackingColor = Color.blue;
-			break;
-		case PossibleColors.Cyan:
-			trackingColor = Color.cyan;
-			break;
-		case PossibleColors.Yellow:
-			trackingColor = new Color(1, 1, 0);
-			break;
-		case PossibleColors.Magenta:
-			trackingColor = Color.magenta;
-			break;
-		}
-
-		adjustedColor = new Vector3 (trackingColor.r, trackingColor.g, trackingColor.b);
-
-		bool cMY = adjustedColor.sqrMagnitude > 1 ? true : false;
-
-		for (int i = 0; i < 3; i++) {
-			if (adjustedColor [i] <= 0)
-				adjustedColor [i] = -1;
-			if (cMY && adjustedColor [i] == 1)
-				adjustedColor [i] = 0.5f;
-		}
-
-		Debug.Log (adjustedColor);
-
-		testTexture.material.SetVector ("_AdjustColor", adjustedColor);
-
-	}
-
-	Vector3 ReadWebCamData(){
+	void ReadWebCamData(){
 		numberPixelsAboveThreshold = 0;
 		int xTotal = 0;
 		int yTotal = 0;
@@ -154,13 +87,13 @@ public class LightPositionMapper : MonoBehaviour {
 		for (int i = 0; i < webCamTex.width; i += PIXEL_INCREMENT) {
 			for (int j = 0; j < webCamTex.height; j += PIXEL_INCREMENT) {
 				Color webCamCol = webCamTex.GetPixel(i, j);
-				float lightness = (webCamCol.r * adjustedColor.x) 
-					+ (webCamCol.g * adjustedColor.y) 
-					+ (webCamCol.b * adjustedColor.z);
+				float lightness = (webCamCol.r + webCamCol.g + webCamCol.b) / 3;
+				Debug.Log ("Lightness: " + lightness + "  GrayScale: " + webCamCol.grayscale);
 				if (lightness > saturationThreshold) {
 					numberPixelsAboveThreshold++;
 					xTotal += i;
 					yTotal += j;
+//					webCamTex.SetPixel(
 				} 
 			}
 		}
@@ -171,15 +104,12 @@ public class LightPositionMapper : MonoBehaviour {
 //		outputTexture.material.SetTexture ("_WebCamTexture", webCamTex);
 		#endif
 
-		if (numberPixelsAboveThreshold > 0 && setMax) {
-			int averageX = Mathf.RoundToInt (xTotal / numberPixelsAboveThreshold);
-			int averageY = Mathf.RoundToInt (yTotal / numberPixelsAboveThreshold);
-
-			return GetPositionFromWebCamData( new Vector2 (averageX, averageY));
-		}
-			
-		return Vector3.zero;
-
+//		if (numberPixelsAboveThreshold > 0) {
+//			int averageX = Mathf.RoundToInt (xTotal / numberPixelsAboveThreshold);
+//			int averageY = Mathf.RoundToInt (yTotal / numberPixelsAboveThreshold);
+//
+//			return GetPositionFromWebCamData( new Vector2 (averageX, averageY));
+//		}
 	}
 
 	Vector3 GetPositionFromWebCamData(Vector2 webCamCoords){
@@ -193,9 +123,7 @@ public class LightPositionMapper : MonoBehaviour {
 		xPos = (xPos * xRange) + MIN_X;
 		yPos = (yPos * yRange) + MIN_Y;
 
-		float zPos = GetDepthPosition ();
-
-		return new Vector3 (-xPos, yPos, zPos);
+		return new Vector3 (-xPos, yPos, 0);
 	}
 
 	void AdjustSaturationThreshold(){
@@ -208,38 +136,4 @@ public class LightPositionMapper : MonoBehaviour {
 			testTexture.material.SetFloat("_SaturationThreshold", saturationThreshold);
 		}
 	}
-
-	float GetDepthPosition(){
-		float zRange = MAX_Z - MIN_Z;
-
-		float zPos = Mathf.Clamp01((float)numberPixelsAboveThreshold / ((float)maxSaturation - (float)minSaturation));
-	
-		zPos = (zPos * zRange) + MIN_Z;
-
-		return zPos;
-	}
-
-	void SetZDepthRange(){
-		if (setMin && !setMax && Input.GetKeyDown (KeyCode.Space)) {
-			maxSaturation = numberPixelsAboveThreshold;	
-			setMax = true;
-			instructionsText.text = "Hold Space to paint texture. WASD keys move viewpoint. Q & E change paint color.";
-			StartCoroutine (SetInstructionsTextInactive ());
-		}
-		if (!setMin && Input.GetKeyDown (KeyCode.Space)) {
-			minSaturation = numberPixelsAboveThreshold;
-			setMin = true;
-			arrowInstructionsText.SetActive (false);
-			instructionsText.text = "Hold tracking object close to camera and press space to set FAR plane value.";
-		}
-		if (setMin && setMax && Input.GetKeyUp (KeyCode.Space)) {
-			ProceduralMesh.GetInstance().canPaint = true;
-		}
-	}
-
-	IEnumerator SetInstructionsTextInactive(){
-		yield return new WaitForSeconds (5f);
-		instructionsText.gameObject.SetActive (false);
-	}
-
 }
---
+++ Assets/WebCamShader.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WebCamShader : MonoBehaviour {
+
+	WebCamTexture webCamTex;
+
+	bool initialized = false;
+
+	//Shader stuff
+	Material renderMat;
+	[SerializeField] [Range(0,1)] float saturationThreshold;
+	[SerializeField] Color renderLightColor;
+
+	//Reading shader texture
+	[SerializeField] int texReadIncrement = 4;
+
+	// Use this for initialization
+	void Awake () {
+		webCamTex = new WebCamTexture ();
+		webCamTex.Play();
+		renderMat = new Material (Shader.Find ("Hidden/DrawRed"));
+		renderMat.SetFloat ("_SaturationThreshold", saturationThreshold);
+		renderMat.SetColor ("_RenderColor", renderLightColor);
+
+		initialized = true;
+	}
+
+	void OnValidate(){
+		if (initialized) {
+			renderMat.SetFloat ("_SaturationThreshold", saturationThreshold);
+			renderMat.SetColor ("_RenderColor", renderLightColor);
+		}
+	}
+
+	// Postprocess the image
+	void OnRenderImage (RenderTexture source, RenderTexture destination)
+	{
+		if (initialized) {
+			renderMat.SetTexture ("_WebCamTex", webCamTex);
+
+			Graphics.Blit (source, destination, renderMat);
+
+			Texture renderedTex = renderMat.mainTexture;
+
+			for (int i = 0; i < renderedTex.width; i += texReadIncrement) {
+				for (int j = 0; j < renderedTex.height; j += texReadIncrement) {
+
+				}
+			}
+		}
+	}
+}
