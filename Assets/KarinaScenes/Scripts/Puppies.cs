3d963db2f4f4d2ff04d8ce30c1c5ab1e89482518
krpopp <karina.popp@nyu.edu>
2018-02-21 12:54:43 -0500

    Dalmatian gives birth to dumb babies

---
+++ Assets/KarinaScenes/Scripts/Puppies.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Puppies : MonoBehaviour {
+
+	public float speed;
+	public Vector3 vel;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		vel = Random.insideUnitSphere * speed;
+		//vel.y = 0.0f;
+		transform.Translate (vel * Time.deltaTime);
+	}
+}
