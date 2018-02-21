e5198f8cf1163a4afffa739e768d6c6cd40d38c6
krpopp <karina.popp@nyu.edu>
2018-02-21 11:11:51 -0500

    Moar

---
+++ Assets/KarinaScenes/Scripts/BirthPuppies.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BirthPuppies : MonoBehaviour {
+
+	public static List<GameObject> puppies = new List<GameObject> ();
+	//static GameObject aPup;
+
+	public int pupCount; 
+
+	// Use this for initialization
+	void Start () {
+		//aPup = Resources.Load ("Puppy") as GameObject;
+		GiveBirth ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void GiveBirth(){
+		for (int i = 0; i <= pupCount; i++) {
+			puppies.Add (Instantiate ((Resources.Load ("Puppy")) as GameObject, gameObject.transform.position, Quaternion.identity));
+		}
+	
+	}
+
+}
---
+++ Assets/KarinaScenes/Scripts/ZapBodies.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZapBodies : MonoBehaviour {
+
+	//This script should be on all snake middles
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter2D(Collider2D other){
+		//replace this check with whatever duncan is doing
+		if (other.gameObject.tag == "PixelColliders") {
+			RemoveSnakeBodies ();
+		}
+	}
+
+	//run this when the middle of snake body hits light
+	void RemoveSnakeBodies(){
+		SnakeTest.middles.Remove (gameObject);
+	}
+}
