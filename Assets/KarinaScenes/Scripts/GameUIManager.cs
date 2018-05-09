e7535e5591acd7398e6e754f23ca6a810ffc473d
krpopp <karina.popp@nyu.edu>
2018-05-09 17:45:54 -0400

    Background changes and sum UI

---
+++ Assets/KarinaScenes/Scripts/GameUIManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameUIManager : MonoBehaviour {
+
+	public Text scoreText;
+	public Text timeText;
+	ScoreScript scoreScript;
+
+
+	// Use this for initialization
+	void Start () {
+		GameObject scoreManager = GameObject.Find("ScoreManager");
+		scoreScript = scoreManager.GetComponent<ScoreScript>();	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		scoreText.text = "SCORE: " + scoreScript.score;
+		timeText.text = string.Format("{0}:{1:00}", (int)scoreScript.time / 60, (int)scoreScript.time % 60);
+	}
+}
---
+++ Assets/KarinaScenes/Scripts/ScoreScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreScript : MonoBehaviour {
+
+
+	public static ScoreScript instance = null;
+	public int score = 0;
+	public float time = 0f;
+
+	void Awake(){
+		if (instance == null) {
+			instance = this;
+		} else if (instance != this) {
+			Destroy (gameObject);
+		}
+	}
+		
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		time += Time.deltaTime;
+	}
+}
